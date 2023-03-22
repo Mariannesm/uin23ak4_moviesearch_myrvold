8b6bdfcb6708503637f4f851be93354e071b10c2
Mariannemyrvold <marmyr6@hotmail.com>
2023-03-22 17:04:59 +0100

    Endret ut ifra API objectet

--- arbeidskrav/src/compontents/MovieCard.js
+++ arbeidskrav/src/compontents/MovieCard.js
@@ -1,6 +1,10 @@
-export default function MovieCard () {
+export default function MovieCard ({poster, title, year}) {
     return (
-        <>
-        </>
+                <article className="movie-card">
+                    <img src={poster} alt={title} />
+                    <h3>{title}</h3>
+                    <p>{year}</p>
+                </article>
+        
     )
 }
--- arbeidskrav/src/compontents/MovieCards.js
+++ arbeidskrav/src/compontents/MovieCards.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 export default function MovieCards () {
 
     const [SearchResult, setSearchResult] = useState([])
-    const [Search, setSearch] = useState("James+bond")
+    const [Search, setSearch] = useState("James+Bond")
 
     
         const movies  = async() =>{
-            const response = await fetch(`http://www.omdbapi.com/?s=${Search}&apikey=b4ca312`)
+            const response = await fetch(`http://www.omdbapi.com/?s=${Search}&type=movie&apikey=b4ca312`)
             const data = await response.json()
             setSearchResult(data)
             console.log(data)
@@ -20,7 +20,7 @@ export default function MovieCards () {
 
     return (
     
-    <>
-    </>
+        <>
+        </>
     )
 }
