8c447b4e4cc268185b876d052e07bc1c97ee4478
Mariannesm <marmyr6@hotmail.com>
2023-03-26 20:58:34 +0200

    Betingelse på  tomt bilde

--- arbeidskrav/src/compontents/MovieCard.js
+++ arbeidskrav/src/compontents/MovieCard.js
@@ -1,10 +1,11 @@
+import React from 'react';
+
 export default function MovieCard ({poster, title, year}) {
     return (
-                <article className="movie-card">
-                    <img src={poster} alt={title} />
-                    <h3>{title}</h3>
-                    <p>{year}</p>
-                </article>
-        
+            <article className="movie-card">
+                <img src={poster != "N/A" ? poster : "https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_960_720.png"} alt={title} />
+                <h3>{title}</h3>
+                <p>{year}</p>
+            </article>
     )
 }
